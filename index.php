c80f89daf224c3281c7da921d15cb3e6d6c1aeeb
marcos tebes <marcosalexistebes@hotmail.com>
2024-09-09 21:14:38 -0300

    clase 09/09/2024

--- index.php
+++ index.php
@@ -3,35 +3,35 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <title>Pizzeria Pizze Il Napolitano</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <!-- Link font awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>PIZZE IL NAPOLITANO</title>
+
 </head>
 
 <body>
     <header>
         <div id="header-container">
             <div id="logo">
-                <img src="img/pizza.svg" alt="logo de pizza">
-                <div class="logo-text">
-                    <img src="img/text.svg" alt="logo de texto ">
-                </div>
+                <img src="img/pizza.svg" alt="">
+                <img class="logo-text" src="img/text.svg" alt="">
+
             </div>
-
-
-
             <nav>
                 <ul>
                     <li><a href="#">HOME</a></li>
@@ -41,72 +41,65 @@
                 </ul>
             </nav>
         </div>
-        <div class="main-content">
-            <h2 class="animate__animated  animate__backInLeft">NUESTRAS PIZZAS</h2>
-            <div id="cart">
-                <i class="fa badge" id="badge" value=0><i class="fa-solid fa-cart-shopping fa-lg"></i></i>
-            </div>
-            <ul class="gallery">
-                <?php
-                include_once("config_products.php");
-                include_once("db.class.php");
-                new Db();
-                $link=new Db();
-                $link->run($sql, NULL);
-                $stmt=$link->run($sql, NULL);
-                $data = $stmt->fetchAll();
-                foreach ($data as $row) 
-                try {
-                    // Conexion a la Base de Datos
-                    $conn = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
-                    //echo "Conexion Exitosa";
-                    $sql = "select c.category_name,p.image,p.product_name,p.price, date_format(p.start_date,'%d/%m/%Y') as date from products p inner join categories c on p.id_category=c.id_category order by p.price";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->execute();
-                    $data = $stmt->fetchAll();
-                } catch (PDOException $e) {
-                    echo "¡Error!: ";
-                    die();
-                }
-                foreach ($data as $row) {
-                ?>
-                    <li>
-                        <div class="box">
-                            <figure>
-                                <img src="<?php echo $row['image'] ?>" alt="fugazzeta" class="img-jpg">
-                                <figcaption>
-                                    <h3><?php echo $row['category_name'] . '' . $row['product_name'] ?></h3>
-                                    <p><?php echo '$' . ' ' . $row['price'] ?></p>
-                                    <time><?php echo $row['date'] ?></time>
-                                </figcaption>
-                            </figure>
-                            <button class="button" value="1">
-                                Añadir al carrito <i class="fa-solid fa-cart-shopping"></i>
-                            </button>
-                        </div>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
-        </div>
-        <footer>
-            <p>Copyright &copy;
-                <script>
-                    document.write(new Date().getFullYear());
-                </script> Todos los derechos reservados
-            </p>
-        </footer>
-        <nav id="social">
-            <a href="#"> <i class="fa-brands fa-twitter"></i></a>
-            <a href="#"><i class="fa-brands fa-facebook"></i></a>
-            <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
-        </nav>
     </header>
-
-
-
-
+    <div class="main-content">
+        <h2 class="animate__animated animate__backInLeft">NUESTRAS PIZZAS</h2>
+        <div id="cart">
+            <i class="fa badge" id="badge" value=0><i class="fa-solid fa-cart-shopping fa-lg"></i></i>
+        </div>
+        <ul class="gallery">
+             <?PHP 
+             include_once("config_products.php");
+             include_once("db.class.php");
+             $link=new Db();
+             $sql="SELECT c.category_name,p.image,p.product_name,p.price,date_format(p.start_date,'%d/%m/%Y') as 'DATE' FROM products p inner join categories c on p.id_category=c.id_category order by p.price";
+             $stmt=$link->run($sql, NULL);
+             $data = $stmt->fetchAll();
+             try {
+                // Conexion a la Base de Datos
+                $conn = new PDO("mysql:host=". SERVER_NAME. ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
+                //echo "Conexion Exitosa";
+                $sql="SELECT c.category_name,p.image,p.product_name,p.price,date_format(p.start_date,'%d/%m/%Y') as 'DATE' FROM products p inner join categories c on p.id_category=c.id_category order by p.price";
+                $stmt= $conn -> prepare ($sql);
+                $stmt->execute();
+                $data=$stmt->fetchAll();
+                }
+                catch (PDOException $e) {
+                  echo "¡Error!: ";
+                  die();
+                }
+                foreach ($data as $row){
+              ?>
+            <li>
+                <div class="card"> <!-- card es box -->
+                    <figure> <img src="<?PHP echo $row['image']?>" class="img-jpg" alt="fugazzeta" />
+                        <figcaption>
+                            <h3><?PHP echo $row['category_name']." ".$row['product_name']?></h3>
+                            <p><?PHP echo "$". " " . $row ['price']?></p>
+                            <time><?PHP echo $row['DATE']?></time>
+                        </figcaption>
+                        <button class="button" value="1">
+                            Añadir al carrito <i class="fa-solid fa-cart-shopping"></i>
+                        </button>
+                    </figure>
+                </div>
+            </li>
+            <?PHP
+                }
+            ?>
+        </ul>
+    </div>
+    <footer>
+        <p>Copyright &copy;
+            <script> document.write(new Date().getFullYear()); </script> Todos los derechos reservados
+        </p>
+    </footer>
+    <nav id="social">
+        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+    </nav>
+    <script></script>
     <script src="js/main.js"></script>
 </body>
 
